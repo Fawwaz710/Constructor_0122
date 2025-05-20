a48b14a723b5e2dc8ea581d069e2bda78ab30abb
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-05-20 18:34:41 +0700

    membuat atribut

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+//buatlah program c++ dengan class barang yang memiliki atribut: namaBarang,
+//kode barang
+//program memiliki construktor berparameter untuk mengisi
+//nilai namaBarang dan kodeBarang.
+//serta memiliki fungsi untuk menampilkan  informasi barang 
+
+//nama repository latihanConstruktor_0122
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class barang {
+    public:
+    string namaBarang;
+    int kodeBarang;
+};
